7c037fb1ab67561e75f5fdedef6f65c8661d3c9d
ArunGovil <a.arungovil@gmail.com>
2023-01-08 19:30:38 +0530

    update/lsp-message

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -11,22 +11,45 @@ lsp.ensure_installed({
 })
 
 lsp.set_preferences({
+        cmp_capabilities = true,
+        configure_diagnostics = true,
+        manage_nvim_cmp = true,
         sign_icons = {
                 error = 'E',
                 warn = 'W',
-                info = 'I'
+                info = 'I',
+                hint = "H"
         }
 })
 
 lsp.configure('tsserver', {
-  on_attach = function(client, bufnr)
-  end,
-  filetypes = { "typescript", "typescriptreact", "typescript.tsx", "typescript.ts" },
-  settings = {
-    completions = {
-      completeFunctionCalls = true
-    }
-  }
+        on_attach = function(client, bufnr)
+        end,
+        filetypes = { "typescript", "typescriptreact", "typescript.tsx", "typescript.ts" },
+        settings = {
+                completions = {
+                        completeFunctionCalls = true
+                }
+        }
 })
 
+lsp.configure('vuels', {})
+
 lsp.setup()
+
+vim.opt.signcolumn = 'yes'
+vim.diagnostic.config({
+        virtual_text = true,
+        signs = true,
+        update_in_insert = false,
+        underline = true,
+        severity_sort = false,
+        float = {
+                focusable = false,
+                style = 'minimal',
+                border = 'rounded',
+                source = 'always',
+                header = '',
+                prefix = '',
+        },
+})
