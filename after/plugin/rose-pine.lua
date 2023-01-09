86cab129a5f49dfcabcad9c38bdfad4c82f6491e
ArunGovil <a.arungovil@gmail.com>
2023-01-09 18:00:43 +0530

    theme/config

---
+++ after/plugin/rose-pine.lua
@@ -0,0 +1,7 @@
+require("rose-pine").setup({
+	groups = {
+		background = "none",
+	},
+})
+
+vim.cmd("colorscheme rose-pine")
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>ls", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>y", "\"+y")
-vim.keymap.set("v", "<leader>y", "\"+y")
-vim.keymap.set("n", "<leader>Y", "\"+Y")
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>Y", '"+Y')
