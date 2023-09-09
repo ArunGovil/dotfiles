0ecdecfb93e295540dd75b0373c15218326685e5
ArunGovil <a.arungovil@gmail.com>
2023-09-09 23:56:35 +0530

    Migrate to lazy nvim

--- lua/base.lua
+++ lua/base.lua
@@ -1,10 +1,8 @@
 vim.scriptencoding = "utf-8"
 vim.opt.encoding = "utf-8"
 vim.opt.fileencoding = "utf-8"
-
 vim.opt.nu = true
 vim.opt.relativenumber = true
-
 vim.opt.shell = "bash"
 vim.opt.title = true
 vim.opt.autoindent = true
@@ -17,10 +15,10 @@ vim.opt.termguicolors = true
 vim.opt.background = "dark"
 vim.opt.scrolloff = 10
 vim.opt.laststatus = 2
-
 vim.opt.cursorline = true
 vim.opt.termguicolors = true
 vim.opt.winblend = 0
 vim.opt.background = "dark"
 vim.opt.wildoptions = "pum"
 vim.opt.pumblend = 5
+vim.cmd [[colorscheme nightfly]]
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -9,6 +9,16 @@ vim.keymap.set("n", "<leader>s", "G") -- Move to End
 vim.keymap.set("n", "<leader>w", "gg") -- Move to Start
 vim.keymap.set("n", "<leader>e", "$") -- Line end
 vim.keymap.set("n", "<leader>q", "^") -- Line start
+vim.keymap.set("n", "q", 'b', { noremap =true }) -- Move one word back
 vim.keymap.set("n", "<leader>c", vim.cmd.noh) -- Clear find
 vim.keymap.set("n", "<leader>x", '"_dd') -- Delete without yank
 vim.keymap.set("n", "<leader>r", "cgn") -- Replace text
+vim.keymap.set("n", "<leader>z", ':vsplit<CR>', { noremap =true }) -- Split pane vertical
+vim.keymap.set("n", "<leader>,", ':split<CR>', { noremap =true }) -- Split pane horizontal
+vim.keymap.set('n', '<leader><Tab>', '<C-w>l', { noremap = true }) -- Move to right pane
+vim.keymap.set('n', '<leader>.', '<C-w>k', { noremap = true }) -- Move to top pane
+vim.keymap.set('n', '<leader>\\', '<C-w>h', { noremap = true }) -- Move to left pane
+vim.keymap.set('n', '<leader><CR>', '<C-w>j', { noremap = true }) -- Move to bottom pane
+vim.keymap.set('n', "<leader>f", [[<cmd>Telescope find_files<cr>]], { noremap = true, silent = true }) -- Find files
+vim.keymap.set('n', "<leader>g", [[<cmd>Telescope live_grep<cr>]], { noremap = true, silent = true }) -- Live grep
+vim.keymap.set('n', "<leader>b", [[<cmd>Telescope buffers<cr>]], { noremap = true, silent = true }) -- Find buffers
